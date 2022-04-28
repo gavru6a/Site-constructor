d14a122f4130d1caba72d8f2b2a0ed202202a054
gavru6a <97871216+gavru6a@users.noreply.github.com>
2022-04-28 06:51:47 +0300

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('ROOT', dirname(__FILE__).'/');
-require ("classes/base.class.php");
-$main_page = 'home';
-header('Content-Type: text/html; charset=utf-8');
-include (file_exists(ROOT . "pages/$mode.php"))? (ROOT . "pages/$mode.php"): (ROOT . "pages/404.php"); //For new updates
